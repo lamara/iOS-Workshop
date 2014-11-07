5a14ad9db115512f2a42e8b4584755e63850f5e1
Alex Lamar <alexlamar@Alexs-MacBook-Pro-2.local>
2014-11-07 16:04:45 -0500

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Alex Lamar on 11/7/14.
+//  Copyright (c) 2014 Alex Lamar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDoList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDoList
+//
+//  Created by Alex Lamar on 11/7/14.
+//  Copyright (c) 2014 Alex Lamar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
